acd696dc5cb44b1560666c845f17ec421d24d419
afiqos <mdafiqosman@gmail.com>
2020-08-05 11:51:05 +0800

    Creating new resource using POST API

--- src/main/java/app/topic/TopicService.java
+++ src/main/java/app/topic/TopicService.java
@@ -2,16 +2,17 @@ package app.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(     // Added "new Arraylist<>" so topics becomes mutable
             new Topic("spring", "Spring framework", "Spring Framework Description"),
             new Topic("java", "Core java", "Core java Description")
-        );
+        ));
 
     public List<Topic> getAllTopics() {
         return topics;
@@ -19,5 +20,9 @@ public class TopicService {
 
     public Topic getTopic(String id) {
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
