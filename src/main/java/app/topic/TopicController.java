47b783ccf81f5481f3ae0c48805b41ba6ae552e4
afiqos <mdafiqosman@gmail.com>
2020-08-05 10:16:20 +0800

    First commit

---
+++ src/main/java/app/topic/TopicController.java
@@ -0,0 +1,19 @@
+package app.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("spring", "Spring framework", "Spring Framework Description"),
+                new Topic("java", "Core java", "Core java Description")
+        );
+    }
+}
